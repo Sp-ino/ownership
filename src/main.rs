fe30bf76559aeb71ef09e32dba303e7e20ff0b3b
sp-ino <spinovale97@gmail.com>
2022-10-27 10:20:37 +0200

    Add more experiments with ownership

--- src/main.rs
+++ src/main.rs
@@ -82,22 +82,91 @@ fn main() {
         // 63 |         println!("s is {s}");
         // |                         ^ value borrowed here after move
         // |
+
+        // ADDITIONAL CONSIDERATIONS ABOUT CASTING AN IMMUTABLE VARIABLE 
+        // AS MUTABLE WHEN PASSING IT TO A FUNCTION
+        // I can pass an immutable variable to a function that has a
+        // signature like the following:
+        // 
+        // fn myfunc(mut myarg: String) //obviously we can have other data types
+        // 
+        // By doing so, the variable becomes mutable when it is moved to
+        // the scope of the function. For example,
+        let new_s = String::from("hello ");
+        take_and_return_ownership_with_mut(new_s);
+
+        // The take_and_return_ownership_with_mut function prints the length of
+        // the argument, appends "world!" to it and then prints the new length.
+        // Finally, it returns the modified string.
+        // Question: is the new_s variable still valid after passing it to the function?
+        // 
+        // println!("This is the value of new_s: {new_s}"); //compile error!
+        // 
+        // Answer: NO!
+        // Yet, I can't bind new_s to the return value of the function either.
+        // If I had written
+        //
+        // new_s = take_and_return_ownership_with_mut(new_s); //compile error!
+        // 
+        // I would have also produced a compile error.
+        // Instead, it works with shadowing:
+
+        let new_s = String::from("hello ");
+        let new_s = take_and_return_ownership_with_mut(new_s);
+        println!("This is the value of new_s: {new_s}");
+
     }
     // --------------------------------------------------------------------------
 
+    // ---------------Experiments with borrowing and references------------------
+    {
+        println!("\n\nSCOPE 4");
+        let mystr = String::from("Ziopera");
+
+        // In this case I can still use mystr after passing it to print_with_borrowing,
+        // because we only pass a reference to the function
+        print_len_with_borrowing(&mystr);
+
+        let mut another_str = String::from("ziopera");
+
+        // I can pass the reference to a *mutable* variable to a function that expects a mutable
+        // reference and then use the variable after it has been modified by
+        // the function
+        append_with_borrowing(&mut another_str);
+        println!("another_str after calling append_with_borrowing on it: {another_str}");
+
+        // But I cannot pass a reference to an immutable variable to a function that expects
+        // a mutable reference:
+        // 
+        // append_with_borrowing(&mut mystr); //compile error!
+    // --------------------------------------------------------------------------
+    }
 }
 
 
-fn print_incremented_int(mut arg: i32) {
+fn print_incremented_int(arg: i32) {
     // when this function is called the variable that is passed to it,
     // which resides in the stack, is copied into another variable,
     // that also resides in the stack
-    arg = arg + 1;
-    println!("print_incremented_int:argument incremented by one is: {arg}"); 
+    let res = arg + 1;
+    println!("print_incremented_int:argument incremented by one is: {res}"); 
 }
 
 
-fn print_len_with_ownership(mut arg: String) {
+fn print_len_with_ownership(arg: String) {
+    // when this function is called the variable that is passed to it
+    // is *moved* into the scope of the function and becomes invalid in the scope
+    // from which it has been passed. This means that the triplet
+    // (address, len, capacity) in the stack is moved from the scope
+    // of the caller to the scope of the callee, while the heap region in which the
+    // actual string is stored remains unchanged when the ownership of the
+    // variable changes
+    let len = arg.len();
+    println!("print_len_with_ownership:length of string {}: {}", arg, len);
+}
+
+
+fn take_and_return_ownership_with_mut(mut arg: String) -> String{
     // when this function is called the variable that is passed to it
     // is *moved* into the scope of the function and becomes invalid in the scope
     // from which it has been passed. This means that the triplet
@@ -106,10 +175,25 @@ fn print_len_with_ownership(mut arg: String) {
     // actual string is stored remains unchanged when the ownership of the
     // variable changes
     let mut len = arg.len();
-    println!("print_len_with_ownership:length of argument string: {len}");
+    println!("take_and_return_ownership_with_mut:length of string {}: {}", arg, len);
 
-    // I'm not allowed to modify
+    // I'm allowed to modify, if I specify that the argument
+    // is passed as mutable
     arg.push_str("world!");
     len = arg.len();
-    println!("print_len_with_ownership:length of argument string after adding 'world!' to it: {len}");
+    println!("take_and_return_ownership_with_mut:length of {} after adding 'world!' to it: {}", arg, len);
+
+    arg
+}
+
+
+fn print_len_with_borrowing(s: &String) {
+
+    let len = s.len();
+    println!("print_len_with_borrowing:the string {} has length {}", s, len);
+}
+
+
+fn append_with_borrowing(s: &mut String) {
+    s.push_str(" fra!");
 }
